99276e4ffdbbc4a198d1b1276da9a92bc2fc8acc
guilherme <guimedeiroxdev@gmail.com>
2024-03-02 23:26:12 -0300

    feat: creating main menu

---
+++ src/main/java/org/example/oopproject/MainController.java
@@ -0,0 +1,24 @@
+package org.example.oopproject;
+
+public class MainController {
+
+
+
+    //    @FXML
+//    private Label welcomeText;
+//
+//    @FXML
+//    private Label modifiedText;
+//
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Hello World");
+//    }
+//
+//    @FXML
+//    protected void onHelloCustomButtonClick() {
+//        welcomeText.setText("This is a custom message");
+//        modifiedText.setText("Modified Text");
+//    }
+
+}
