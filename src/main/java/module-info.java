08ee4769177665da8575ea00c9e63cf20e432c7e
guilherme <guimedeiroxdev@gmail.com>
2024-03-03 23:11:52 -0300

    feat: tracker screen + some functions

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,10 @@ module org.example.oopproject {
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires okhttp3;
 
     opens org.example.oopproject to javafx.fxml;
     exports org.example.oopproject;
+    exports org.example.oopproject.Controller;
+    opens org.example.oopproject.Controller to javafx.fxml;
 }
---
+++ src/main/java/org/example/oopproject/Controller/TrackerController.java
@@ -0,0 +1,28 @@
+package org.example.oopproject.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class TrackerController{
+
+    @FXML
+    private Button buttonBackToMain;
+
+    @FXML
+    private Button buttonTrackingPacket;
+
+    @FXML
+    private TextField labelTrackingCode;
+
+    @FXML
+    private AnchorPane sceneTrackingPane;
+
+    @FXML
+    void trackPackage(ActionEvent event) {
+
+    }
+
+}
